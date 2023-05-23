d49f8eea3af583e8194149d1fd0c5758d6ae12f6
Ekaterina Shchegoleva <kateg@list.ru>
2023-05-23 16:14:58 +0300

    1

---
+++ src/services/reducers/reducers.test.js
@@ -0,0 +1,27 @@
+import {changeConstructor} from './reducers'
+import * as types from '../actions/actions'
+
+
+it('should handle ADD_TODO', () => {
+    const state= {
+        main: [],     
+        orders: [],    
+      };
+      const action={
+        type: types.ADD_CONSTRUCTOR,
+        payload: {_id: '12', name:'cucumber'},
+        key: 'abc',   
+        orders: ['12']  
+      }
+      let newState=changeConstructor(state, action)
+    expect(newState).toEqual({
+        main: [
+            {
+                _id: '12', 
+                name:'cucumber',  
+                key: 'abc', 
+            }
+        ],   
+        orders: ['12'],  
+      })
+})
